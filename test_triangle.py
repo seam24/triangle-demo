fa27a95d3b42b5b542060cb40e8d4c4a4e3a1d10
jjsohn92 <kasiossh555@gmail.com>
2025-05-07 14:33:52 +0900

    Update tests

--- test_triangle.py
+++ test_triangle.py
@@ -4,3 +4,9 @@ def test_invalid1():
     assert triangle(-1, 0, 1) == -1
 def test_isosceles():
     assert triangle(3, 2, 3) == 1
+
+def test_equaliteral():
+    assert triangle(3, 3, 3) == 2 
+    
+def test_invalid2():
+    assert triangle(2, 3, 5) == -1
