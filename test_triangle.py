46812106c5b84f5dbbd932403f65103216e023c9
jjsohn92 <kasiossh555@gmail.com>
2026-04-23 09:36:35 +0900

    add a new test

--- test_triangle.py
+++ test_triangle.py
@@ -3,6 +3,9 @@ from triangle import triangle
 def test_invalid1():
     assert triangle(-1, 0, 1) == -1
 
+def test_invalid2():
+    assert triangle(0, 0, 1) == -1
+
 def test_isosceles():
     assert triangle(3, 2, 3) == 1
 
